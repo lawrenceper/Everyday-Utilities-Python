927e0878883d0372a278bf0c73aa1b0bd80d81fc
Lawrence Perez <lawrenceperez2010@gmail.ccom>
2016-08-06 18:38:49 -0600

    Added python scripts for a stopwatch and a timer. There are plans to add the ability to enter and display hours, minutes, and seconds, although I'm not sure how this can be accomplished. These applications can be run on you Windows, Mac, Linux and even iOS if you have an instalation of Python 2.7X. On iOS, make sure that you have an app that can .py extentions of python. Also, make sure that you have a Python 2.7X app.
    You may contribute by fixing bugs and translating the code into Python 3X code, and translating them into other languages like Spanish.

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+print("Welcome to Open Timer.")
+x = 0
+while x == 0:
+    sec = input("Enter the number of seconds.")
+    while sec > 0:
+        print(sec)
+        time.sleep (1.00)
+        sec = sec-1
+
+    print (0)
+    print("Timer Done.")
